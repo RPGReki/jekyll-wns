014b06b5b85cd265b97c540c9c29ddfff7f4da77
Thai “0xReki” Chung <mail@0xreki.de>
2021-06-06 17:54:27 +0200

    Add quads filter to avoid duplicate code

--- lib/jekyll-wns/wns-quads.rb
+++ lib/jekyll-wns/wns-quads.rb
@@ -7,28 +7,39 @@ class Quads
   def self.enabled (doc)
     wns = doc.site.config["wns"] || {}
     quads = wns["quads"] || false
+
     return quads
+  end
+ 
+  def self.quads (input)
+    output = input.gsub(Quads::MAIN_EXPRESSION, Quads::MAIN_REPLACEMENT)
+    output = output.gsub(Quads::CLEANUP_EXPRESSION, Quads::CLEANUP_REPLACEMENT)
+
+    return output
   end
 end
 
 Jekyll::Hooks.register :documents, :pre_render do |document|
   if Quads::enabled(document)
-    document.content = document.content.gsub(Quads::MAIN_EXPRESSION, Quads::MAIN_REPLACEMENT)
-    document.content = document.content.gsub(Quads::CLEANUP_EXPRESSION, Quads::CLEANUP_REPLACEMENT)
+    document.content = Quads::quads(document.content)
   end
-
 end
 
 Jekyll::Hooks.register :pages, :pre_render do |document|
   if Quads::enabled(document)
-    document.content = document.content.gsub(Quads::MAIN_EXPRESSION, Quads::MAIN_REPLACEMENT)
-    document.content = document.content.gsub(Quads::CLEANUP_EXPRESSION, Quads::CLEANUP_REPLACEMENT)
+    document.content = Quads::quads(document.content)
   end
 end
 
 Jekyll::Hooks.register :posts, :pre_render do |document|
   if Quads::enabled(document)
-    document.content = document.content.gsub(Quads::MAIN_EXPRESSION, Quads::MAIN_REPLACEMENT)
-    document.content = document.content.gsub(Quads::CLEANUP_EXPRESSION, Quads::CLEANUP_REPLACEMENT)
+    document.content = Quads::quads(document.content)
   end
 end
+
+module QuadsFilter
+  def quads (input)
+  end
+end
+
+Liquid::Template.register_filter(QuadsFilter)
