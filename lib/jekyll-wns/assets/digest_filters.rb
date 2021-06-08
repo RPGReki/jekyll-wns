4a2ceaa4c3958a83c9a2ca52b57f63d41157e4d9
Thai “0xReki” Chung <mail@0xreki.de>
2021-06-08 15:04:23 +0200

    Check for nil case

--- lib/jekyll-wns/assets/digest_filters.rb
+++ lib/jekyll-wns/assets/digest_filters.rb
@@ -2,19 +2,35 @@ require 'digest/sha2'
 module WNS
   module DigestFilters
     def sha256_64(input)
-      Digest::SHA256.base64digest input
+      unless input.nil?
+        Digest::SHA256.base64digest input
+      else 
+        ""
+      end
     end
 
     def sha384_64(input)
-      Digest::SHA384.base64digest input
+      unless input.nil?
+        Digest::SHA384.base64digest input
+      else 
+        ""
+      end
     end
 
     def sha256(input)
-      Digest::SHA256.hexdigest input
+      unless input.nil?
+       Digest::SHA256.hexdigest input
+      else 
+        ""
+      end
     end
 
     def sha384(input)
-      Digest::SHA384.hexdigest input
+      unless input.nil?
+        Digest::SHA384.hexdigest input
+      else 
+        ""
+      end
     end
   end
 end
