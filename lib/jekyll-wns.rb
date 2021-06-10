6c0b2b70ae677442521a5e7dcb517b3994f5b437
Thai “0xReki” Chung <mail@0xreki.de>
2021-06-10 15:45:26 +0200

    Add missing require

--- lib/jekyll-wns.rb
+++ lib/jekyll-wns.rb
@@ -8,6 +8,7 @@ require_relative 'jekyll-wns/assets/digest_filters'
 require_relative 'jekyll-wns/assets/file_properties_filter'
 require_relative 'jekyll-wns/assets/quads_filter'
 require_relative 'jekyll-wns/assets/scene_break_tag'
+require_relative 'jekyll-wns/assets/youtube_tag'
 
 require_relative 'jekyll-wns/register_filters'
 require_relative 'jekyll-wns/register_hooks'
