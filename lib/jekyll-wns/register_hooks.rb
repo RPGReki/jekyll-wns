92185abc85a42f3be6ba1ed886a39c70cc8640bd
Thai “0xReki” Chung <mail@0xreki.de>
2021-06-17 13:35:44 +0200

    Improve Abbreviations & Label Unicode code

--- lib/jekyll-wns/register_hooks.rb
+++ lib/jekyll-wns/register_hooks.rb
@@ -1,21 +1,23 @@
 Jekyll::Hooks.register :pages, :pre_render do |document|
   if document.path =~ /(.md|.html)/
+    WNS::Abbreviations::replace_abbreviations! document
+    WNS::LabelUnicode::label_unicode! document
     WNS::Quads::insert_quads_into_document! document
   end
 end
 
 Jekyll::Hooks.register :pages, :post_render do |document|
   if document.path =~ /(.md|.html)/
-    WNS::Abbreviations::replace_abbreviations! document
-    WNS::LabelUnicode::label_unicode! document
+    true
   end
 end
 
 Jekyll::Hooks.register :posts, :pre_render do |document|
+  WNS::Abbreviations::replace_abbreviations! document
+  WNS::LabelUnicode::label_unicode! document
   WNS::Quads::insert_quads_into_document! document
 end
 
 Jekyll::Hooks.register :posts, :post_render do |document|
-  WNS::Abbreviations::replace_abbreviations! document
-  WNS::LabelUnicode::label_unicode! document
+  true
 end
