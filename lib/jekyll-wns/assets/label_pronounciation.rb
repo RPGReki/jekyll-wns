1819da33a11d9540caa4e64cb69c4f27bec5c930
Thai Chung <mail@0xreki.de>
2022-03-29 18:03:07 +0200

    Fix Type Error

--- lib/jekyll-wns/assets/label_pronounciation.rb
+++ lib/jekyll-wns/assets/label_pronounciation.rb
@@ -9,7 +9,7 @@ module WNS
 
     def self.label_pronounciation!(doc)
       wns = doc.site.config["wns"] || {}
-      labels = wns["label_pronounciation"] || false
+      labels = wns["label_pronounciation"] || []
       for i in (0..(labels.length()) - 1) do
         doc.content = doc.content.gsub(Regexp.new("\b(#{labels[i]["term"]})\b"), "<span aria-label='#{labels[i]["pronounciation"]}'>\\1</span>")
       end
